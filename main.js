6bad451264252a7563ef65b5623db63501b22b10
nowshad-hossain-rahat <nowshad.hossain.rahat@gmail.com>
2022-03-10 23:05:08 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", (evt) => {
+
+    let bodyScrollHeight = document.body.scrollHeight;
+    let i = 10;
+
+    window.addEventListener('scroll', (evt) => {
+
+        i = window.scrollY;
+
+    });
+
+    let scroller = setInterval(() => {
+
+        if(i >= bodyScrollHeight){clearInterval(scroller); return;}
+        console.log('now : ', i);
+        window.scrollTo(0, i);
+        i+=10;
+
+    }, 100);
+
+});
